8f0e7e4fc10de4330d0a430c419f4b29027bee5b
Hanqnero <57258124+Hanqnero@users.noreply.github.com>
2021-02-20 17:24:48 +0500

    Update logic.py

--- pyqt-gui/logic.py
+++ pyqt-gui/logic.py
@@ -1,15 +1,6 @@
 from subprocess import Popen, PIPE
 from os import path, system
 
-
-#def start_download(delete, format, url, chapters, bar):
-#    path_to_grabber = path.join(path.dirname(__file__), 'grabber.exe')
-#    grabber = Popen([path_to_grabber, 
-#        f'-url={url}', 
-#        f'{"-delete" if delete else ""}',
-#        format,
-#        chapters], shell=True, stdout=PIPE,)                       #starting grabber
-
 def start_download(delete, format, url, chapters):
     path_to_grabber = path.join(path.dirname(__file__), 'grabber.exe')
     system(f'"{path_to_grabber}" -url={url} {"-delete" if delete else ""} {format} {chapters}')
